374e1861922f80162ba231e4ef2b1e58ed383af6
IrvanYusuf <irvanyusufcahyadi4@gmail.com>
2025-02-03 20:33:34 +0700

    update session

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 },
+    cookie: { secure: true, maxAge: 24 * 60 * 60 * 1000 },
   })
 );
 
